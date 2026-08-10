42ab48b3de3072b96e4c5d0e9a4af887a9a8e290
Jamal-GOAT <nathanhendrawansusanto@gmail.com>
2026-08-10 21:17:25 +0700

    DATA PACKETS DONE and continuing transmission method

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // fade in effect
-const els = document.querySelectorAll('.home-section2 .wrapper div');
+const els = document.querySelectorAll('.home-section2 .wrapper div, .dp-section1 .cards .card');
 
 function isInScreen() {
     els.forEach(el => {
@@ -17,3 +17,26 @@ function isInScreen() {
 window.addEventListener('scroll', isInScreen);
 
 isInScreen();
+//break
+//click card
+const cards = document.querySelectorAll('.dp-section2 .cards .card');
+
+function isClicked(el) {
+    el.currentTarget.classList.toggle('clicked');
+}
+
+cards.forEach(card => {
+    card.addEventListener('click', isClicked);
+})
+//break
+//switch
+const switchBox = document.querySelector('.serial-vs-parallel .card'); 
+const buttons = document.querySelectorAll('.serial-vs-parallel .card .switch div');
+
+buttons[0].addEventListener("click", () => {
+    switchBox.className = 'card serial'
+})
+
+buttons[1].addEventListener("click", () => {
+    switchBox.className = 'card parallel'
+})
